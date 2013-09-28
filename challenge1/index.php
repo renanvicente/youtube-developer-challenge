206be2c8c9a18e4006a580187fd7590584497835
Leandro Toledo <leandrotoledodesouza@gmail.com>
2013-09-28 12:44:28 -0300

    PHP lib

--- challenge1/index.php
+++ challenge1/index.php
@@ -1,76 +1,76 @@
 <?php
 if ($_GET['q'] && $_GET['maxResults']) {
-  // Call set_include_path() as needed to point to your client library.
-  require_once ($_SERVER["DOCUMENT_ROOT"].'/API/youtube/google-api-php-client/src/Google_Client.php');
-  require_once ($_SERVER["DOCUMENT_ROOT"].'/API/youtube/google-api-php-client/src/contrib/Google_YouTubeService.php');
+    // Call set_include_path() as needed to point to your client library.
+    require_once 'google-api-php-client/src/Google_Client.php';
+    require_once 'google-api-php-client/src/contrib/Google_PlusService.php';
 
-  /* Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
-  Google APIs Console <http://code.google.com/apis/console#access>
-  Please ensure that you have enabled the YouTube Data API for your project. */
-  $DEVELOPER_KEY = 'AIzaSyDOkg-u9jnhP-WnzX5WPJyV1sc5QQrtuyc';
+    /* Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
+    Google APIs Console <http://code.google.com/apis/console#access>
+    Please ensure that you have enabled the YouTube Data API for your project. */
+    $DEVELOPER_KEY = 'AIzaSyDOkg-u9jnhP-WnzX5WPJyV1sc5QQrtuyc';
 
-  $client = new Google_Client();
-  $client->setDeveloperKey($DEVELOPER_KEY);
+    $client = new Google_Client();
+    $client->setDeveloperKey($DEVELOPER_KEY);
 
-  $youtube = new Google_YoutubeService($client);
+    $youtube = new Google_YoutubeService($client);
 
-  try {
-    $searchResponse = $youtube->search->listSearch('id,snippet', array(
-      'q' => $_GET['q'],
-      'maxResults' => $_GET['maxResults'],
-    ));
+    try {
+        $searchResponse = $youtube->search->listSearch('id,snippet', array(
+            'q' => $_GET['q'],
+            'maxResults' => $_GET['maxResults'],
+        ));
 
-    $videos = '';
-    $channels = '';
+        $videos = '';
+        $channels = '';
 
-    foreach ($searchResponse['items'] as $searchResult) {
-      switch ($searchResult['id']['kind']) {
-        case 'youtube#video':
-          $videos .= sprintf('<li>%s (%s)</li>', $searchResult['snippet']['title'],
-            $searchResult['id']['videoId']."<a href=http://www.youtube.com/watch?v=".$searchResult['id']['videoId']." target=_blank>   Watch This Video</a>");
-          break;
-        case 'youtube#channel':
-          $channels .= sprintf('<li>%s (%s)</li>', $searchResult['snippet']['title'],
-            $searchResult['id']['channelId']);
-          break;
-       }
+        foreach ($searchResponse['items'] as $searchResult) {
+            switch ($searchResult['id']['kind']) {
+                case 'youtube#video':
+                    $videos .= sprintf('<li>%s (%s)</li>', $searchResult['snippet']['title'],
+                        $searchResult['id']['videoId']."<a href=http://www.youtube.com/watch?v=".$searchResult['id']['videoId']." target=_blank>     Watch This Video</a>");
+                    break;
+                case 'youtube#channel':
+                    $channels .= sprintf('<li>%s (%s)</li>', $searchResult['snippet']['title'],
+                        $searchResult['id']['channelId']);
+                    break;
+             }
+        }
+
+     } catch (Google_ServiceException $e) {
+        $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+            htmlspecialchars($e->getMessage()));
+    } catch (Google_Exception $e) {
+        $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+            htmlspecialchars($e->getMessage()));
     }
-
-   } catch (Google_ServiceException $e) {
-    $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
-      htmlspecialchars($e->getMessage()));
-  } catch (Google_Exception $e) {
-    $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
-      htmlspecialchars($e->getMessage()));
-  }
 }
 ?>
 
 <!doctype html>
 <html>
-  <head>
-    <title>YouTube Search</title>
-	<style type="text/css">
-		body{margin-top: 50px; margin-left: 50px}
-	</style>
-  </head>
-  
-  <body>
-	<form method="GET">
-		
-		<div>
-			Search:<input type="text" name="search">
-		</div>
-		
-		<button>Pesquisar!</button>
-		
-	</form>
-	
-	<div id="resultado">
-		<!-- Resultado da pesquisa -->
-		<h3> Resultado </h3>
-		<ul><?php echo $videos; ?></ul>
-	</div>
+    <head>
+        <title>YouTube Search</title>
+    <style type="text/css">
+        body{margin-top: 50px; margin-left: 50px}
+    </style>
+    </head>
 
-	</body>
+    <body>
+    <form method="GET">
+
+        <div>
+            Search:<input type="text" name="search">
+        </div>
+
+        <button>Pesquisar!</button>
+
+    </form>
+
+    <div id="resultado">
+        <!-- Resultado da pesquisa -->
+        <h3> Resultado </h3>
+        <ul><?php echo $videos; ?></ul>
+    </div>
+
+    </body>
 </html>
